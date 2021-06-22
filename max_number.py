20e44b4eb7ec761c5d1f0e75e37eadacf0eda065
amritat123 <83055726+amritat123@users.noreply.github.com>
2021-06-22 12:08:29 +0530

    Update max_number.py

--- max_number.py
+++ max_number.py
@@ -1,9 +1,9 @@
 def max_num(max_1):
     i=0
-    max_2=0
+    max=0
     while i< len(max_1,):
-        if max_1[i]>0:
-            max_2=max_1[i]
+        if max_1[i]>max:
+            max=max_1[i]
         i+=1
-    return (max_2)
+    return (max)
 print(max_num([1,2,3,4,5,6,7]))
